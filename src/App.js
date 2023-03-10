bb45c99989e4b481aeac00cfe841e645ffcebd7c
bnardi99 <47116754+bnardi99@users.noreply.github.com>
2023-03-09 19:57:37 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      This project was coded by Brenda Nardi and is {""}
-      <a href="https://reactjs.org" target="_blank">
-        open-sourced on GitHub.
-      </a>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Brenda Nardi and is {""}
+          <a
+            href="https://github.com/bnardi99/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
